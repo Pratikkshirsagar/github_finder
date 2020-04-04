817bee42d6a51d1d48cacf2e8d9f1b3e7a53ce77
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-04 19:18:23 +0530

    config added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -18,6 +17,8 @@ import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import PageNotFound from './pages/PageNotFound';
 import { UserContext } from './context/userContext';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -26,12 +27,14 @@ function App() {
     <Router>
       <ToastContainer />
       <UserContext.Provider value={{ user, setUser }}>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/signin" component={Signin} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="*" component={PageNotFound} />
         </Switch>
+        <Footer />
       </UserContext.Provider>
     </Router>
   );
